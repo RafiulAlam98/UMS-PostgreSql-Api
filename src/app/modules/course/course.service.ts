772f62567567aacfbed4bc4967521f3d8e9c73f4
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-09-25 00:21:33 +0600

    ADDED: Course and Course Prerequisite Transaction roleback part

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,14 +1,60 @@
-import {Course, PrismaClient} from "@prisma/client";
+import { PrismaClient} from "@prisma/client";
+import ApiError from "../../../errors/ApiError";
+import httpStatus from "http-status";
+import {ICourseCreateData} from "./course.interface";
 
 
 const prisma = new PrismaClient()
 
-const insertIntoDb = async (payload:Course):Promise<Course> =>{
-    const result = await prisma.course.create({
-        data:payload
+const insertIntoDb = async (payload:any):Promise<any> =>{
+    const {preRequisiteCourses,...courseData} = payload
+
+    const newCourse = await prisma.$transaction(async (transactionClient)=>{
+        const result = await transactionClient.course.create({
+            data:courseData
+        })
+
+        if(!result){
+            throw  new ApiError(httpStatus.BAD_REQUEST,"Unable To Create Course")
+        }
+
+        if (preRequisiteCourses && preRequisiteCourses.length > 0){
+            for (let index =0; index< preRequisiteCourses.length ;index++){
+                const createPreRequisite = await transactionClient.courseToPrerequisite.create({
+                    data:{
+                        courseId:result.id,
+                        preRequisiteId:preRequisiteCourses[index].courseId
+                    }
+                })
+                console.log(createPreRequisite)
+            }
+        }
+        return result
     })
-    console.log(result)
-    return result
+
+if(newCourse){
+    const responseData = await  prisma.course.findUnique({
+        where:{
+            id:newCourse.id
+        },
+        include:{
+            preRequisite:{
+                include:{
+                    preRequisite: true
+                },
+            },
+            preRequisiteFor:{
+                include: {
+                    course:true
+                }
+            }
+        }
+    })
+    return responseData
+}
+
+    throw  new ApiError(httpStatus.BAD_REQUEST,"Unable To Create Course")
+
 }
 
 export const CourseService = {
