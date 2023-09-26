6c200a0b552b407e36635f3dcf057d9b15a80344
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-09-26 22:48:16 +0600

    UPDATED: Semester Registration Validation Status

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,7 +1,27 @@
-import {SemesterRegistration} from "@prisma/client";
+import {SemesterRegistration, SemesterRegistrationStatus} from "@prisma/client";
 import prisma from "../../../shared/prisma";
+import ApiError from "../../../errors/ApiError";
+import httpStatus from "http-status";
 
 const insertIntoDb = async (data:SemesterRegistration):Promise<SemesterRegistration> =>{
+    const isAnySemRegUpcomingOrOngoing = await prisma.semesterRegistration.findFirst({
+        where:{
+            OR:[
+                {
+                    status:SemesterRegistrationStatus.UPCOMING
+                },
+                {
+                    status:SemesterRegistrationStatus.ONGOING
+                }
+            ]
+        }
+    })
+
+    if(isAnySemRegUpcomingOrOngoing){
+        throw  new ApiError(httpStatus.BAD_REQUEST,
+            "Registration is Ongoing or upcoming")
+    }
+
     const result = await prisma.semesterRegistration.create({
         data
     })
