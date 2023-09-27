ad22564402be764aad83ef280f259368fd4f5064
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-09-27 20:48:24 +0600

    UPDATED: OfferedCourseClassSchedule Conflict Part

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,3 +1,6 @@
+import {Weekdays} from "@prisma/client";
+import ApiError from "../errors/ApiError";
+import httpStatus from "http-status";
 
 export const asyncForEach = async (array:any[], callBack:any)=>{
     if(!Array.isArray(array)){
@@ -6,4 +9,26 @@ export const asyncForEach = async (array:any[], callBack:any)=>{
     for(let index = 0; index < array.length; index++){
         await callBack(array[index], index,array)
     }
+}
+
+export const hasTimeConflict = (existingSlot:{
+    startTime:string,
+    endTime:string,
+    dayOfWeek:Weekdays
+    }[], newSlot:{
+    startTime:string,
+    endTime:string,
+    dayOfWeek:Weekdays
+}) =>{
+    for(const slot of existingSlot){
+        const existingStart = new Date(`1970-01-01T${slot.startTime}`)
+        const existingEnd = new Date(`1970-01-01T${slot.endTime}`)
+        const newStart = new Date(`1970-01-01T${newSlot.startTime}`)
+        const newEnd = new Date(`1970-01-01T${newSlot.endTime}`)
+
+        if(newStart < existingEnd && newEnd > existingStart){
+            return true
+        }
+    }
+    return false
 }
